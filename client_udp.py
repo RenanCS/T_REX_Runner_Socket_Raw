542262203006ecf7d25b5316f736aaa407e2cfd2
Renan_Carvalho <renancarvalhodesouza@gmail.com>
2017-09-07 10:51:12 -0300

    comunication client and server

--- client_udp.py
+++ client_udp.py
@@ -1,16 +1,100 @@
+#!/usr/bin/python
+import socket
+import time
+import os
+import threading
+import sys
+
+clear = lambda: os.system('clear')
+alive = True
+scene = ''
+
+def draw(scene):
+    clear()
+    print 'received draw: ' + scene
+
+def gameLoop():
+    global alive, scene
+    alive = True
+    while alive:
+        alive, scene = getSetDataServer(0)    
+        draw(scene)
+        time.sleep(0.2)
+
+def readKeypress():
+    global alive
+    while alive:
+        raw_input()
+        alive, scene = getSetDataServer(1)
+        sendServerData()
+
+def getSetDataServer(action):
+    
+    global state
+
+    msg = str(action) + ',' +  str(state)
+
+    state += 1
+    
+    sock.send(msg)
+    
+    draw = sock.recv(1024)
+    
+    return True, draw
+
+    #return True, '---'
+
+def sendServerData():
+    sock.send(1)
+    # send the jump to server
+    print 'sent jump'
+
+def main():    
+    global alive
+    global sock
+    global state 
+
+    state = 0
+       
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = 'localhost' #raw_input("Server hostname or ip? ")
+    port = 1096 #input("Server port? ")
+    #sock.connect((host,port))
+
+    sock.connect(('127.0.0.1', 1098))
+        
+    try:
+        t=threading.Thread(target=gameLoop)
+        t2=threading.Thread(target=readKeypress)
+        t.daemon = True
+        t.start()
+        t2.daemon = True
+        t2.start()
+    except:
+        print "Error: unable to start thread"
+    
+    while alive:
+        pass
+    print "\nGAME OVER !!!\n"
+    
+
+if  __name__ =='__main__':main()
+
+
+
+'''
 import socket
 
-HOST = '192.168.1.10'  # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = 'localhost' #raw_input("Server hostname or ip? ")
+port = 1096 #input("Server port? ")
+#sock.connect((host,port))
 
-udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.connect(('127.0.0.1', 1098))
 
-dest = (HOST, PORT)
-print 'Para sair use CTRL+X\n'
+while True:
+    data = raw_input("message: ")
+    sock.send(data)
+    print "response: ", sock.recv(1024)
 
-msg = raw_input()
-
-while msg <> '\x18':
-    udp.sendto (msg, dest)
-    msg = raw_input()
-udp.close()
+'''
--- server_udp.py
+++ server_udp.py
@@ -1,31 +1,153 @@
-from socket import socket, AF_INET, SOCK_RAW, SOCK_STREAM
-from struct import unpack
-from binascii import hexlify
-
-HOST = ''              # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
+import socket
+import threading
+import struct
 
 
-rawsocket = socket(AF_INET, SOCK_RAW)
-rawsocket.bind(("192.168.2.4", 8000))
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM,socket.IPPROTO_TCP)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
 
-#udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.scene = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0]
+        self.jumping = False
+        self.countJump = 0
+        self.alive = True
+        self.countJump = 5
+        self.unpacker = struct.Struct('I I')
 
-orig = (HOST, PORT)
-udp.bind(orig)
 
-while True:
-	request = rawsocket.recvfrom(2048)
-    header = request[0][0:14]
-	hd = unpack("!6s6s2s", header)
-	
-	dest_addr = hexlify(hd[0])
-	source_addr = hexlify(hd[1])
-	type = hexlify(hd[2])
-	
-	print "destination: {0}".format(dest_addr)
-	print "source: {0}".format(source_addr)
-	print "destination: {0}".format(type)
-	#msg, cliente = udp.recvfrom(1024)
-    print cliente, msg
-udp.close()
+    def draw(self):
+
+        global input
+        scene_str = "\n"*20
+        first = self.scene.pop(0)
+        self.scene.append(first)
+        
+        for (i, e) in enumerate(self.scene):
+
+            if self.jumping:
+                scene_str += "o"
+            else:
+                scene_str += " "
+
+
+        scene_str += "\n"
+    
+        for (i, e) in enumerate(self.scene):
+            
+            print(i,self.jumping,self.countJump)
+
+            if self.jumping:
+                self.countJump = self.countJump - 1    
+        
+            if self.countJump == 0:
+                self.jumping = False
+            
+            if(i == 5 and e == 1 and not self.jumping):
+                scene_str += "x"
+                self.alive = False
+            elif i == 5 and not self.jumping:
+                scene_str += "o"
+            elif e == 0:
+                scene_str += "_"
+            else:
+                scene_str += "|"
+            
+    
+        return scene_str
+
+
+    def openPackage(self):
+        
+        print('chegou aqui')
+
+        data = self.sock.recv(1024)
+
+        header = data[0][0:14]    
+        ipHeader = data[0][14:34]    
+        tcpHeader = data[0][34:54]  
+
+        hd = struct.unpack("!6s6s2s", header)    
+        
+       
+        print('---------- ETHERNET FRAME ----------')    
+        print('Destination mac: {0}', hexlify(hd[0]))    
+        print('Source mac: {0}', hexlify(hd[1])     )  
+        print('Type: {0}', hexlify(hd[2])           )
+    
+        ip_hrd = struct.unpack("!12s4s4s",ipHeader)    
+    
+        print('---------- IP ----------'                          )    
+        print('Source IP: {0}', socket.inet_ntoa(ip_hrd[1])       )  
+        print('Destination IP: {0}', socket.inet_ntoa(ip_hrd[2])  )  
+        
+        #tcp_hrd = struct.unpack("!HH16s",tcpHeader)    
+    
+        tcp_hrd = struct.unpack("!HHLLBBHHH",tcpHeader)    
+        
+        print('---------- TCP ----------'         )   
+        print('Source Port: {0}', tcp_hrd[0]      )
+        print('Destination Port: {0}', tcp_hrd[1] )   
+        print('Flag: {0}',tcp_hrd[2]              )
+        print('Acknowledgement : {0}', tcp_hrd[3] )   
+        print('TCP header length: {0}',tcp_hrd[4] )
+
+        return
+
+
+    def listen(self):
+
+        self.sock.listen(5)
+
+        while True:
+
+            client, address = self.sock.accept()
+
+            #self.openPackage()
+           
+            client.settimeout(20)
+
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+
+    def listenToClient(self, client, address):
+        
+        size = 2048
+
+        while True:
+
+            try:
+                data = client.recv(size)
+
+                if data:
+
+                    #print("data chegou", data)
+
+                    if int(data.split(',')[0]) == 1:
+                        print("PULO",data.split(',')[0])
+                        self.jumping = True;
+
+                    # Set the response to echo back the recieved data 
+                    response = self.draw()
+
+                    client.send(response)
+                else:
+                    raise error('Client disconnected')
+                
+            except:
+                client.close()
+                return False
+
+
+if __name__ == "__main__":
+    while True:
+        #port_num =  input("Port? ")
+        try:
+            port_num = 1098 #int(port_num)
+            break
+        except ValueError:
+            pass
+
+    ThreadedServer('',port_num).listen()
